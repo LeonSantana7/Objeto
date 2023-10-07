f8a9b7304dcec3cb8ef3a13e65d749c3c014eda1
Leon Santana <leon.santana854@gmail.com>
2023-10-07 15:10:46 -0300

    Atalho no objeto(shorthands)

---
+++ shorthands.js
@@ -0,0 +1,27 @@
+//shorthands = Atalhos
+
+
+const carro = {
+    marca: "toyota",
+    modelo: "etios",
+    ano: 2020,
+    potencia: 80,
+}
+
+const minhaAltura = 1.73;
+const meuNome = "José";
+const meuPeso = 60;
+
+const pessoa = {
+    nome: meuNome,
+    altura: minhaAltura,
+    peso: meuPeso,
+    carro //shorthands
+};
+
+
+// marca
+// modelo
+// ano
+// pontência
+console.log(pessoa);
