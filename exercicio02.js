7cceec242439ded88d7a74d743d8cce49bdbc9de
Leon Santana <leon.santana854@gmail.com>
2023-10-07 16:00:21 -0300

    Alteração no codigo

--- exercicio02.js
+++ exercicio02.js
@@ -5,20 +5,31 @@ const cartao = {
     nomeCliente: "Carlos",
     idade: 30,
     produtos: [{
-            nome: "Celular",
-            preco: 500,
+            nome: "Aguá",
+            preco: 2,
             quantidade: 1,
         },
         {
-            nome: "Computador",
-            preco: 1500,
+            nome: "Picolé",
+            preco: 1.50,
             quantidade: 1,
         },
         {
-            nome: "Tablet",
-            preco: 200,
+            nome: "Refrigerante",
+            preco: 5,
             quantidade: 1,
         }]
 }
 
-console.log(cartao)
+//imprimir o nome do cliente
+//imprimir a idade do cliente
+//modifique a idade do cliente
+//imprima a nova idade do cliente
+//imprima o nome do primeiro produto consumido
+// imprima o preço unitario do ultimo produto consumido por ele
+console.log(`Nome do Cliente: ${cartao.nomeCliente}
+Idade do Cliente: ${cartao.idade}`);
+idade = 25;
+console.log(`Nova idade do Cliente: ${idade}
+Primeiro produto consumido: ${cartao.produtos[0].nome}
+Preço unitario do ultimo produto consumido: ${cartao.produtos[2].preco}`);
